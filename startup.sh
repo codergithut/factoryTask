b9dd1f113a990dfe8470d10be442c73b3a140263
田剑 <tj1468195034@gmail.com>
2020-06-20 13:43:30 +0800

    develp
    Signed-off-by: 田剑 <tj1468195034@gmail.com>

--- startup.sh
+++ startup.sh
@@ -8,8 +8,8 @@ mvn package
 
 mvn dockerfile:build
 
-docker stop facroty_task
+docker stop task
 
-docker rm factory_task
+docker rm task
 
 docker run -d -it -p 8080:8080 --name task tianjian3209/factory_task:develop
