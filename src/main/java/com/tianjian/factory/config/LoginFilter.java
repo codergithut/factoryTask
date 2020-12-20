5262366c2c10eb97d705947aff7c48fb1195035f
田剑 <tj1468195034@gmail.com>
2020-12-20 15:25:42 +0800

    develp
    Signed-off-by: 田剑 <tj1468195034@gmail.com>

--- src/main/java/com/tianjian/factory/config/LoginFilter.java
+++ src/main/java/com/tianjian/factory/config/LoginFilter.java
@@ -28,10 +28,10 @@ public class LoginFilter implements Filter {
 
         HttpServletRequest request = (HttpServletRequest) servletRequest;
         HttpServletResponse response = (HttpServletResponse) servletResponse;
-        if(request.getRequestURI().contains("task") && loginCacheService.getUserIdByRequest(request) == null) {
-            response.setStatus(401);
-            return ;
-        }
+//        if(request.getRequestURI().contains("task") && loginCacheService.getUserIdByRequest(request) == null) {
+//            response.setStatus(401);
+//            return ;
+//        }
         response.addHeader("Access-Control-Allow-Origin", "*");
         String headers = request.getHeader("Access-Control-Request-Headers");
         // 支持所有自定义头
